87f297597819bdc1e018c0e70c66a427920ee44c
Shanchuan Xia <shanchuan.xia@live.com>
2017-02-09 19:27:38 -0500

    xx

--- drawstuff.js
+++ drawstuff.js
@@ -616,15 +616,15 @@ function shadeTriIsect(isect,isectSphere,lights,triangles,spheres) {
                     // add in the diffuse light
                      // var sphereCenter = new Vector(sphere.x,sphere.y,sphere.z);
 
-                    // a = new Vector(triangles.vertices[0][0],triangles[isectSphere].vertices[0][1],triangles[isectSphere].vertices[0][2]);
-                   // 	b= new Vector(triangles.vertices[1][0],triangles.vertices[1][1],triangles[isectSphere].vertices[1][2]);
-                   // 	c = new Vector(triangles.vertices[2][0],triangles.vertices[2][1],triangles[isectSphere].vertices[2][2]);
-                   // 	var ab = Vector.subtract(b,a);
-                   // 	var ac = Vector.subtract(c,a);
-                   // 	var N = Vector.normalize(Vector.cross(ab,ac));
+                    a = new Vector(triangles.vertices[0][0],triangles.vertices[0][1],triangles[isectSphere].vertices[0][2]);
+                   	b= new Vector(triangles.vertices[1][0],triangles.vertices[1][1],triangles[isectSphere].vertices[1][2]);
+                   	c = new Vector(triangles.vertices[2][0],triangles.vertices[2][1],triangles[isectSphere].vertices[2][2]);
+                   	var ab = Vector.subtract(b,a);
+                   	var ac = Vector.subtract(c,a);
+                   	var N = Vector.normalize(Vector.cross(ab,ac));
 
 
-                    var N = new Vector(triangles.normals[isectSphere][0],triangles.normals[isectSphere][1],triangles.normals[isectSphere][2]); // surface normal
+                    /／var N = new Vector(triangles.normals[0][0],triangles.normals[0][1],triangles.normals[0][2]); // surface normal
 
 
                     var diffFactor = Math.max(0,Vector.dot(N,Vector.normalize(L)));
